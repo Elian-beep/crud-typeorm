33e6febabdce5aa977f75dab7e81c86fe5c8201a
Elian-beep <elianoliver101@gmail.com>
2023-07-14 17:29:37 -0300

    :tada: create project

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import "reflect-metadata";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+ app.listen(port, () => console.log(`Server is running in http://localhost:${port}`));
+
