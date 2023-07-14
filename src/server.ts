af89f29a1bae48ab32443848df13b56e98040994
Elian-beep <elianoliver101@gmail.com>
2023-07-14 17:34:32 -0300

    :refactor: create env for local port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
  app.listen(port, () => console.log(`Server is running in http://localhost:${port}`));
 
